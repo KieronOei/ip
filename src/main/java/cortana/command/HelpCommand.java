c57fefaca8e5d44ef66ede8f3ad2acef7d327dee
KieronOei <65146443+KieronOei@users.noreply.github.com>
2025-09-17 15:38:09 +0800

    Add more error handling for dates
    Add a check on whether parsedDate is before current time
    
    Ensure users do not input deadlines or events that are in the past

--- src/main/java/cortana/command/HelpCommand.java
+++ src/main/java/cortana/command/HelpCommand.java
@@ -8,9 +8,9 @@ public class HelpCommand implements Command {
     public String execute(cortana.task.TaskList taskList, cortana.storage.FileHandler fileHandler) {
         return "Here are the available commands:\n"
                 + "-> todo <task_name> - Adds a todo task\n"
-                + "-> deadline <task_name> /by <D MM YY HHmm OR D MMM YY HHmm> - Adds a deadline task\n"
-                + "-> event <task_name> /from <D MM YY HHmm OR D MMM YY HHmm> "
-                + "/to <D MM YY HHmm OR D MMM YY HHmm> - Adds an event task\n"
+                + "-> deadline <task_name> /by <d MM yy HHmm OR d MMM yy HHmm> - Adds a deadline task\n"
+                + "-> event <task_name> /from <d MM yy HHmm OR d MMM yy HHmm> "
+                + "/to <d MM yy HHmm OR d MMM yy HHmm> - Adds an event task\n"
                 + "-> list - Lists all tasks\n"
                 + "-> mark <task_number> - Marks a task as done\n"
                 + "-> unmark <task_number> - Marks a task as not done\n"
